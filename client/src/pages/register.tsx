c845707c22f42b9c1f4b0c46329c4c656baa7d81
rize <pavlov.v@wlbl.ru>
2020-09-07 22:30:08 +0300

    frontend init

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface registerProps {
+
+}
+
+const Register: React.FC<registerProps> = ({}) => {
+    return <div>register page</div>
+}
+
+export default Register
